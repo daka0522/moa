4937b212eee561822f5203aebd8d756619e566e2
rsch <daka0522@gmail.com>
2020-02-09 17:45:40 +0900

    Init

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <section id="homeIntro">
+      <img alt="Moa logo" src="../assets/moa-128.png">
+      <p>Welcome to MOA</p>
+      <p>This website is the collection of usefull application</p>
+    </section>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+
+export default {
+  name: 'Home',
+  
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
